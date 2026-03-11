79f3cf2675d7ceb0b178ca99effbffd1beac1a8d
Pablo Jacobs <pablo.jacobs.02@gmail.com>
2026-03-11 17:00:09 +0100

    Attempt02Start

---
+++ ProcedualDungeon/Assets/Attempt01/Scripts/TileData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+namespace Attempt01
+{
+    [System.Serializable]
+    public class TileData
+    {
+        public GameObject Prefab;
+        public int TileCost;
+    }
+}
--- ProcedualDungeon/Assets/Scripts/TileData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class TileData
-{
-    public GameObject Prefab;
-    public int TileCost;
-}
